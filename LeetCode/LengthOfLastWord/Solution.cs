6a0f71ef530417885194d7e6564bd66471cc3718
sunero4 <anderskloborg@gmail.com>
2018-03-18 18:56:43 +0100

    Two more problems, implemented my first "hard" one.

---
+++ LeetCode/LengthOfLastWord/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.LengthOfLastWord
+{
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            var count = 0;
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (count > 0 && s[i] == ' ')
+                {
+                    break;
+                }
+                if (s[i] != ' ')
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ LeetCode/SlidingWindowMaximum/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode.SlidingWindowMaximum
+{
+    //https://leetcode.com/problems/sliding-window-maximum/description/
+    public class Solution
+    {
+        public int[] MaxSlidingWindow(int[] nums, int k)
+        {
+            if (nums.Length == 0)
+            {
+                return nums;
+            }
+
+            var window = new int[(nums.Length - k) + 1];
+
+            for (int i = 0, n = (nums.Length - k) + 1; i < n; i++)
+            {
+                var windowMax = Int32.MinValue;
+                for (int j = 0; j < k; j++)
+                {
+                    if (nums[j + i] > windowMax)
+                    {
+                        windowMax = nums[i + j];
+                    }
+                }
+                window[i] = windowMax;
+            }
+
+            return window;
+        }
+    }
+}
