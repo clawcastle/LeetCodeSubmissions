6b20fabf8abd08a986d8a94da00eff6222489fbd
sunero4 <anderskloborg@gmail.com>
2018-03-19 13:51:06 +0100

    A couple more solutions. The concept of counting sort seems to be really useful in a lot of cases

---
+++ LeetCode/FindTheDifference/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.FindTheDifference
+{
+    public class Solution
+    {
+        //https://leetcode.com/problems/find-the-difference/description/
+        public char FindTheDifference(string s, string t)
+        {
+            var firstStringSum = 0;
+
+            for (int i = 0, n = s.Length; i < n; i++)
+            {
+                firstStringSum += s[i];
+            }
+
+            for (int i = 0, n = t.Length; i < n; i++)
+            {
+                firstStringSum -= t[i];
+            }
+
+            return (char)(firstStringSum * -1);
+        }
+    }
+}
---
+++ LeetCode/ValidAnagram/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.ValidAnagram
+{
+    //https://leetcode.com/problems/valid-anagram/description/
+    public class Solution
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            var letters = new int[26];
+            var nonMatchingCount = 0;
+
+            for (int i = 0, n = s.Length; i < n; i++)
+            {
+                letters[(s[i] - 97)]++;
+                nonMatchingCount++;
+            }
+
+            for (int i = 0, n = t.Length; i < n; i++)
+            {
+                if (letters[t[i] - 97] != 0)
+                {
+                    letters[t[i] - 97]--;
+                    nonMatchingCount--;
+                }
+            }
+
+            if (nonMatchingCount == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
